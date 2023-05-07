444a69e8511455b70394990875466f89c8887032
MARK RASMUSSEN <ma@paneldrive.com>
2023-05-07 11:52:10 +0200

    Create Includes.php

---
+++ Includes.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('API.php');
+require_once('Consensus.php');
+require_once('Exchange.php');
+require_once('Index.php');
+require_once('Initiator.php');
+require_once('MineBlock.php');
+require_once('Puzzle.php');
+require_once('Responder.php');
+require_once('Sharding.php');
+require_once('TokenExchange.php');
