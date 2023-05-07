5e1cea908517ee8c99a9e2f171e3110055b2824c
MARK RASMUSSEN <ma@paneldrive.com>
2023-05-07 11:06:19 +0200

    Create Sharding.php

---
+++ Sharding.php
@@ -0,0 +1,107 @@
+// Connect to the database
+$pdo = new PDO('mysql:host=localhost;dbname=my_db;charset=utf8mb4', 'username', 'password');
+
+// Create sharding table
+$sql = "CREATE TABLE IF NOT EXISTS Sharding (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    shard_key VARCHAR(255) NOT NULL,
+    shard_name VARCHAR(255) NOT NULL
+)";
+$pdo->exec($sql);
+
+// Create sharding configuration
+$shard_keys = array(
+    '0' => 'User',
+    '1' => 'Address',
+    '2' => 'Transaction',
+    '3' => 'Block',
+    '4' => 'BlockTransaction',
+    '5' => 'MerkleTree',
+    '6' => 'MerkleTreeHash',
+    '7' => 'MerkleTreeProofItem',
+    '8' => 'MerkleTreeProof'
+);
+
+// Insert sharding configuration into sharding table
+foreach ($shard_keys as $shard_key => $shard_name) {
+    $sql = "INSERT INTO Sharding (shard_key, shard_name) VALUES (:shard_key, :shard_name)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':shard_key', $shard_key, PDO::PARAM_INT);
+    $stmt->bindParam(':shard_name', $shard_name, PDO::PARAM_STR);
+    $stmt->execute();
+}
+
+// Create sharded tables
+foreach ($shard_keys as $shard_key => $shard_name) {
+    $sql = "CREATE TABLE IF NOT EXISTS $shard_name$shard_key (
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
+    switch ($shard_name) {
+        case 'User':
+            $sql .= ",
+            username VARCHAR(255) NOT NULL,
+            password_hash VARCHAR(255) NOT NULL,
+            salt VARCHAR(255) NOT NULL,
+            public_key TEXT NOT NULL";
+            break;
+        case 'Address':
+            $sql .= ",
+            user_id INT(11) UNSIGNED NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES User0(id) ON DELETE CASCADE ON UPDATE CASCADE";
+            break;
+        case 'Transaction':
+            $sql .= ",
+            from_address_id INT(11) UNSIGNED NOT NULL,
+            to_address_id INT(11) UNSIGNED NOT NULL,
+            amount DECIMAL(18,8) NOT NULL,
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            hash TEXT NOT NULL,
+            FOREIGN KEY (from_address_id) REFERENCES Address1(id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (to_address_id) REFERENCES Address1(id) ON DELETE CASCADE ON UPDATE CASCADE";
+            break;
+        case 'Block':
+            $sql .= ",
+            version INT(11) NOT NULL,
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            previous_hash TEXT NOT NULL,
+            merkle_root TEXT NOT NULL,
+            difficulty INT(11) NOT NULL,
+            nonce INT(11) NOT NULL,
+            hash TEXT NOT NULL";
+            break;
+        case 'BlockTransaction':
+            $sql .= ",
+            block_id INT(11) UNSIGNED NOT NULL,
+            transaction_id INT(11) UNSIGNED NOT NULL,
+            FOREIGN KEY (block_id) REFERENCES Block3(id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (transaction_id) REFERENCES Transaction2(id) ON DELETE CASCADE ON UPDATE CASCADE";
+            break;
+        case 'MerkleTree':
+            $sql .= ",
+            sharding_id INT(11) UNSIGNED NOT NULL,
+            root_hash TEXT NOT NULL,
+            FOREIGN KEY (sharding_id) REFERENCES Sharding6(id) ON DELETE CASCADE ON UPDATE CASCADE";
+            break;
+        case 'MerkleTreeHash':
+            $sql .= ",
+            merkle_tree_id INT(11) UNSIGNED NOT NULL,
+            index_in_tree INT(11) NOT NULL,
+            hash TEXT NOT NULL,
+            FOREIGN KEY (merkle_tree_id) REFERENCES MerkleTree6(id) ON DELETE CASCADE ON UPDATE CASCADE";
+            break;
+        case 'MerkleTreeProofItem':
+            $sql .= ",
+            merkle_tree_proof_id INT(11) UNSIGNED NOT NULL,
+            index_in_proof INT(11) NOT NULL,
+            is_right TINYINT(1) NOT NULL,
+            hash TEXT NOT NULL,
+            FOREIGN KEY (merkle_tree_proof_id) REFERENCES MerkleTreeProof8(id) ON DELETE CASCADE ON UPDATE CASCADE";
+            break;
+        case 'MerkleTreeProof':
+            $sql .= ",
+            merkle_tree_id INT(11) UNSIGNED NOT NULL,
+            merkle_tree_hash_id INT(11) UNSIGNED NOT NULL,
+            merkle_tree_proof_item_id INT(11) UNSIGNED NOT NULL,
+            FOREIGN KEY (merkle_tree_id) REFERENCES MerkleTree6(id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (merkle_tree_hash_id) REFERENCES MerkleTreeHash6(id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (merkle_tree_proof_item_id) REFERENCES MerkleTreeProofItem7(id) ON DELETE CASCADE ON UPDATE CASCADE)";
