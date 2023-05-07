a5d6f9097782e316d2f7563158e9909843de1f35
MARK RASMUSSEN <ma@paneldrive.com>
2023-05-07 11:13:55 +0200

    Create Exchange.php

---
+++ Exchange.php
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+const CryptoJS = require('crypto-js');
+const dh = require('diffie-hellman');
+const fs = require('fs');
+const WebSocket = require('ratchet');
+const GuzzleHttp = require('guzzlehttp/guzzlehttp/src/Client');
+
+const app = express();
+const client = redis.createClient();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Redis example
+app.get('/redis/:key', (req, res) => {
+  const key = req.params.key;
+  client.get(key, (err, data) => {
+    if (err) throw err;
+    res.send(data);
+  });
+});
+
+app.post('/redis/:key', (req, res) => {
+  const key = req.params.key;
+  const value = req.body.value;
+  client.set(key, value, (err) => {
+    if (err) throw err;
+    res.send('OK');
+  });
+});
+
+// CryptoJS example
+const message = 'Hello, world!';
+const key = 'my-secret-key';
+const encrypted = CryptoJS.AES.encrypt(message, key).toString();
+console.log(`Encrypted message: ${encrypted}`);
+const decrypted = CryptoJS.AES.decrypt(encrypted, key).toString(CryptoJS.enc.Utf8);
+console.log(`Decrypted message: ${decrypted}`);
+
+// DiffieHellman example
+const alice = dh.generateKeys();
+const bob = dh.generateKeys();
+const aliceSecret = alice.computeSecret(bob.getPublicKey());
+const bobSecret = bob.computeSecret(alice.getPublicKey());
+console.log(`Shared secrets match: ${aliceSecret.toString('hex') === bobSecret.toString('hex')}`);
+
+// fs example
+fs.readFile('/path/to/file', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// Ratchet websocket example
+const ws = new WebSocket('wss://example.com');
+ws.on('open', () => {
+  console.log('WebSocket connection opened');
+});
+ws.on('message', (message) => {
+  console.log(`Received message: ${message}`);
+});
+
+// Guzzle HTTP example
+const client = new GuzzleHttp\Client();
+client.get('https://api.example.com/data').then((response) => {
+  console.log(response.body);
+}).catch((error) => {
+  console.error(error);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
