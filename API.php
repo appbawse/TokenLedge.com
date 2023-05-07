e5ff1353f9f4faadd7b91b581c5ac609d76fc38a
MARK RASMUSSEN <ma@paneldrive.com>
2023-05-07 11:04:53 +0200

    Create API.php

---
+++ API.php
@@ -0,0 +1,110 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('./Blockchain');
+const Puzzle = require('./Puzzle');
+const csv = require('csv-parser');
+const crypto = require('crypto');
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const MerkleTree = require('merkle-tree-solidity');
+const app = express();
+
+const blockchain = new Blockchain();
+
+// parse application/json
+app.use(bodyParser.json());
+
+// POST /addblock
+app.post('/addblock', (req, res) => {
+  const { data, previousHash, nonce } = req.body;
+  const block = blockchain.createBlock(data, previousHash, nonce);
+  blockchain.addBlock(block);
+  res.json(block);
+});
+
+// POST /mineblock
+app.post('/mineblock', (req, res) => {
+  const { data, previousHash } = req.body;
+  const block = blockchain.mineBlock(data, previousHash);
+  blockchain.addBlock(block);
+  res.json(block);
+});
+
+// POST /nodes
+app.post('/nodes', (req, res) => {
+  const { encryptedNode, jwtToken, merkleProof } = req.body;
+  const decryptedNode = decrypt(encryptedNode, jwtToken);
+  const root = MerkleTree.fromSolidityProof(merkleProof).getRoot().toString('hex');
+  const isValid = verifyProof(decryptedNode, root, merkleProof);
+  if (isValid) {
+    // exchange node with other party using Socket.io
+    res.json(decryptedNode);
+  } else {
+    res.status(400).json({ message: "Invalid proof" });
+  }
+});
+
+// GET /puzzle
+app.get('/puzzle', (req, res) => {
+  const puzzle = Puzzle.generatePuzzle(); // assuming Puzzle module exports a generatePuzzle function
+  res.json(puzzle);
+});
+
+// POST /csv
+app.post('/csv', (req, res) => {
+  const { filePath } = req.body;
+  const results = [];
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      res.json(results);
+    });
+});
+
+// POST /privatekey
+app.post('/privatekey', (req, res) => {
+  const { publicKey, prime } = req.body;
+  const sharedSecret = generateSharedSecret(publicKey, prime); // assuming DiffieHellman module exports a generateSharedSecret function
+  // use shared secret to encrypt and decrypt messages between parties
+  res.json(sharedSecret);
+});
+
+// GET /amount
+app.get('/amount', (req, res) => {
+  const { partyId, password } = req.body;
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'my_db',
+  });
+  connection.connect();
+  connection.query(
+    `SELECT amount FROM private_key_index WHERE party_id = '${partyId}' AND password = '${password}'`,
+    (error, results, fields) => {
+      if (error) throw error;
+      res.json(results[0]);
+    }
+  );
+  connection.end();
+});
+
+// Helper functions
+
+function decrypt(encryptedNode, jwtToken) {
+  const privateKey = crypto.randomBytes(32);
+  const decrypted = crypto.privateDecrypt(privateKey, encryptedNode);
+  const payload = jwt.verify(jwtToken, privateKey);
+  return { payload, decrypted };
+}
+
+function generateSharedSecret(publicKey, prime) {
+  const diffieHellman = crypto.createDiffieHellman(prime);
+  const sharedSecret = diffieHellman.computeSecret(publicKey);
+  return sharedSecret;
+}
+
+// Start the server
+app.listen(3000, () => console.log('Server started on port 3000'));
+
