0f7b35d19650861d9f008eaa5457ee3f88271145
MARK RASMUSSEN <ma@paneldrive.com>
2023-05-07 11:18:03 +0200

    Create Puzzle.php

---
+++ Puzzle.php
@@ -0,0 +1,38 @@
+// Add block to Keychain Services
+function addBlock($blockData) {
+  $keychain = new Keychain();
+  $keychain->addGenericPassword('block', json_encode($blockData));
+}
+
+// Mine new block
+function mineBlock($lastBlock, $newData) {
+  $nonce = 0;
+  while (true) {
+    $hash = sha256($lastBlock['hash'] . $newData . $nonce);
+    if (substr($hash, 0, 4) === '0000') {
+      return array(
+        'nonce' => $nonce,
+        'hash' => $hash
+      );
+    }
+    $nonce++;
+  }
+}
+
+// Exchange blocks
+function exchangeBlocks($myBlock, $peerBlock) {
+  if ($peerBlock['hash'] == $myBlock['hash']) {
+    return true;
+  } elseif ($peerBlock['nonce'] > $myBlock['nonce']) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Retrieve puzzles from Keychain Services
+function getPuzzles() {
+  $keychain = new Keychain();
+  $puzzles = $keychain->getGenericPassword('puzzles');
+  return json_decode($puzzles);
+}
